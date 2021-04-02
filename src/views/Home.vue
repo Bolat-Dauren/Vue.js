28b8e3c60c2ac8f7505b00c0e5757f16909b1250
Mochalka <64541687+Mochalka@users.noreply.github.com>
2021-04-02 16:51:45 +0600

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
